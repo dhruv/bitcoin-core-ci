eea8c678df8c2f26abd184dc10d964d042144fea
dhruv <856960+dhruv@users.noreply.github.com>
2022-02-15 17:53:18 -0800

    refactor: Add InitP2P() to NetEventsInterface

--- src/net.h
+++ src/net.h
@@ -792,6 +792,9 @@ public:
     /** Initialize a peer (setup state, queue any initial messages) */
     virtual void InitializeNode(CNode* pnode) = 0;
 
+    /** Initialize the P2P protocol with a peer */
+    virtual void InitP2P(CNode& pnode, bool peer_tx_relay) = 0;
+
     /** Handle removal of a peer (clear state) */
     virtual void FinalizeNode(const CNode& node) = 0;
 
@@ -994,7 +997,7 @@ public:
     //!
     //! The data returned by this is used in CNode construction,
     //! which is used to advertise which services we are offering
-    //! that peer during `net_processing.cpp:PushNodeVersion()`.
+    //! that peer during `net_processing.cpp:InitP2P()`.
     ServiceFlags GetLocalServices() const;
 
     uint64_t GetMaxOutboundTarget() const;
