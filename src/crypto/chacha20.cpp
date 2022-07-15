bitcoin/bitcoin

Adding forward secure FSChaCha20

src/crypto/chacha20.cpp
@@ -5,8 +5,10 @@
 // Based on the public domain implementation 'merged' by D. J. Bernstein
 // See https://cr.yp.to/chacha.html.
 
-#include <crypto/common.h>
 #include <crypto/chacha20.h>
+#include <crypto/common.h>
+#include <hash.h>
+#include <support/cleanse.h>
 
 #include <string.h>
 
@@ -420,5 +422,22 @@ void ChaCha20::Crypt(const unsigned char* m, unsigned char* c, size_t bytes)
         c += 64;
         m += 64;
         prev_block_start_pos = 0;
+    }
+}
+
+void FSChaCha20::Crypt(Span<const std::byte> input, Span<std::byte> output)
+{
+    assert(input.size() == output.size());
+    c20.Crypt(reinterpret_cast<const unsigned char*>(input.data()),
+              reinterpret_cast<unsigned char*>(output.data()), input.size());
+    messages_with_key++;
+
+    if (messages_with_key % rekey_interval == 0) {
+        auto new_key = Hash(rekey_salt, key);
+        memory_cleanse(key.data(), key.size());
+        memcpy(key.data(), &new_key, FSCHACHA20_KEYLEN);
+        c20.SetKey(reinterpret_cast<unsigned char*>(key.data()), key.size());
+        rekey_counter++;
+        set_nonce();
     }
 }

dhruv/bitcoin-core-ci

src/crypto/chacha20.cpp
@@ -5,8 +5,10 @@
 // Based on the public domain implementation 'merged' by D. J. Bernstein
 // See https://cr.yp.to/chacha.html.
 
-#include <crypto/common.h>
 #include <crypto/chacha20.h>
+#include <crypto/common.h>
+#include <crypto/sha256.h>
+#include <span.h>
 
 #include <string.h>
 
@@ -420,5 +422,25 @@ void ChaCha20::Crypt(const unsigned char* m, unsigned char* c, size_t bytes)
         c += 64;
         m += 64;
         prev_block_start_pos = 0;
+    }
+}
+
+void FSChaCha20::Crypt(Span<const std::byte> input, Span<std::byte> output)
+{
+    assert(input.size() == output.size());
+    c20.Crypt(reinterpret_cast<const unsigned char*>(input.data()),
+              reinterpret_cast<unsigned char*>(output.data()), input.size());
+    messages_with_key++;
+
+    if (messages_with_key % rekey_interval == 0) {
+        unsigned char new_key[CSHA256::OUTPUT_SIZE];
+        auto hasher = CSHA256().Write(UCharCast(rekey_salt.data()), rekey_salt.size());
+        hasher.Write(UCharCast(key.data()), key.size()).Finalize(new_key);
+        memory_cleanse(key.data(), key.size());
+        assert(CSHA256::OUTPUT_SIZE == FSCHACHA20_KEYLEN);
+        memcpy(key.data(), new_key, FSCHACHA20_KEYLEN);
+        c20.SetKey(reinterpret_cast<unsigned char*>(key.data()), key.size());
+        rekey_counter++;
+        set_nonce();
     }
 }